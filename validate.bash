e7aa31c689014b450b5ee16d04574c07b463b498
luis <luis.jimenez@uclm.es>
2024-09-27 12:54:13 +0200

    partiendo de la capeta raiz

--- validate.bash
+++ validate.bash
@@ -1,9 +1,13 @@
 #!/bin/bash
-cd sokoban-test/test1
+dirtest=1 # Cambiar este valor al número de la carpeta de test
+finput=test$dirtest/input.txt
+foutput=test$dirtest/salida.txt
+pathbin=bin # Cambiar este valor si la carpeta de ejecutables es distinta
+
 
 # Verifica si los archivos de entrada y resultado existen
-if [ ! -f "input.txt" ] || [ ! -f "salida.txt" ]; then
-  echo "Error: 'input.txt' o 'salida.txt' no se encuentran."
+if [ ! -f $finput ] || [ ! -f $foutput ]; then
+  echo "Error: "$finput" o "$foutput" no se encuentran."
   exit 1
 fi
 
@@ -18,33 +22,28 @@ ntest=1
 # Itera sobre cada línea de input.txt
 while IFS= read -r parametro; do
   # Ejecuta el comando con el parámetro actual
-  salida=$(../bin/sokoban validate -l "$parametro" 2>&1)
+  salida=$($pathbin/sokoban validate -l "$parametro" 2>&1)
   
   # Extrae las líneas correspondientes de resultado.txt
-  resultado_esperado=$(sed -n "${linea_actual},$(($linea_actual + $numlineas - 1))p" salida.txt)
-
-    #echo "salida:" $ntest ":" $parametro
-    #echo $salida
-    #echo "resultado"
-    #echo $resultado_esperado
+  resultado_esperado=$(sed -n "${linea_actual},$(($linea_actual + $numlineas - 1))p" $foutput)
     
-    ntest=$(($ntest+1))
+  ntest=$(($ntest+1))
   
   # Compara la salida con el resultado esperado
   if [ "$salida" != "$resultado_esperado" ]; then
     #echo "Error: El resultado del comando para el parámetro '$parametro' no coincide."
     valid=false
-    echo $ntest ":" $parametro >>../../error.txt
-    echo "salida:" >>../../error.txt
-    echo $salida >>../../error.txt
-    echo "resultado" >>../../error.txt
-    echo $resultado_esperado >>../../error.txt
+    echo $ntest ":" $parametro >>error_$dirtest.txt
+    echo "salida:" >>error_$dirtest.txt
+    echo $salida >>error_$dirtest.txt
+    echo "resultado" >>error_$dirtest.txt
+    echo $resultado_esperado >>error_$dirtest.txt
   fi
   
   # Actualiza la línea actual para la siguiente comparación
   linea_actual=$(($linea_actual + $numlineas))
 
-done < "input.txt"
+done < $finput
 
 # Mensaje final
 if [ "$valid" = true ]; then
