5468e1d0f97e51195bf7f11c16cf8a37bffe91d3
luis <luis.jimenez@uclm.es>
2024-09-30 18:35:25 +0200

    actalizados

--- prueba/test.bash
+++ prueba/test.bash
@@ -8,6 +8,11 @@ tl=7
 
 nl=0
 # Lee el archivo línea por línea y ejecuta el comando con cada línea como parámetro
+
+#borrar fichero de salida si existe
+if [  -f "$archivo_out" ]; then
+  rm $archivo_out
+fi
 while IFS= read -r parametro; do
   #salida=$(./sokoban validate -l "$parametro" 2>&1)
   #echo $salida>>salida2.txt
